86416d2b4ff5868c2bdbc489cd98aa631f62b623
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-05-14 12:45:35 +0300

    Make Constants constructor private

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -4,7 +4,7 @@ public final class Constants {
     public static final long millisInYear = 31556952000L;
     public static final long millisInDay = 86400000L;
     public static final long centsInEuro = 100L;
-    public Constants() {
+    private Constants() {
         throw new UnsupportedOperationException("This is a static utility class and cannot be instantiated");
     }
 }
